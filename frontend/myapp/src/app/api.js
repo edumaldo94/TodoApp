d726a718e9a42b7f939ef2a296f3ba0df56342bf
Eduardo <maldonado19994@gmail.com>
2025-02-06 00:12:57 -0300

    asdasd

--- frontend/myapp/src/app/api.js
+++ frontend/myapp/src/app/api.js
@@ -2,47 +2,47 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: 'https://192.168.0.107:5000/api',
 });
 
 export const getTasks = async () => {
-  try {
-    const response = await api.get('/tasks');
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching tasks:', error);
-    throw error;
-  }
-};
-
-export const addTask = async (task) => {
-  try {
-    const response = await api.post('/tasks', task);
-    return response.data;
-  } catch (error) {
-    console.error('Error adding task:', error);
-    throw error;
-  }
-};
-
-export const updateTask = async (id, task) => {
-  try {
-    const response = await api.put(`/tasks/${id}`, task);
-    return response.data;
-  } catch (error) {
-    console.error('Error updating task:', error);
-    throw error;
-  }
-};
-
-export const deleteTask = async (id) => {
-  try {
-    const response = await api.delete(`/tasks/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error deleting task:', error);
-    throw error;
-  }
-};
-
-export default api;
+    try {
+      const response = await api.get('/tasks');
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+      throw error;
+    }
+  };
+  
+  export const addTask = async (task) => {
+    try {
+      const response = await api.post('/tasks', task);
+      return response.data;
+    } catch (error) {
+      console.error('Error adding task:', error);
+      throw error;
+    }
+  };
+  
+  export const updateTask = async (id, task) => {
+    try {
+      const response = await api.put(`/tasks/${id}`, task);
+      return response.data;
+    } catch (error) {
+      console.error('Error updating task:', error);
+      throw error;
+    }
+  };
+  
+  export const deleteTask = async (id) => {
+    try {
+      const response = await api.delete(`/tasks/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error('Error deleting task:', error);
+      throw error;
+    }
+  };
+  
+  export default api;
