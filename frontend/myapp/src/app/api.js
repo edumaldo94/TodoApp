1302e74165f0bf9db41f881a1513ba26a3569ae4
Eduardo <maldonado19994@gmail.com>
2025-02-08 10:13:11 -0300

    sdf

--- frontend/myapp/src/app/api.js
+++ frontend/myapp/src/app/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: 'https://todoapp2-in5c.onrender.com'
+ baseURL: 'https://todoapp2-in5c.onrender.com/api'
 });
 
 export const getTasks = () => api.get('/tasks');
